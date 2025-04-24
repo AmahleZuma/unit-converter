215dfd5a2021bd07631616bfe54e06cdb7ce367a
Amahle <amahlez798@gmail.com>
2025-04-24 22:51:40 +0200

    Meters to feet calculation input. Learnt about Null safety as this error kept popping up if i added 'toFloatOrNull', which was cool

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,7 @@ fun main() {
     val rule1 = "1. Meters -> feet"
     val rule2 = "2. Kg -> pounds"
     val rule3 = "3 °C -> °F"
+    val unitHeading = "Input the number you wish to convert: "
     println(message)
     println(ruleHeading)
     println(rule1)
@@ -17,7 +18,11 @@ fun main() {
 
     // User Input being checked + actual app logic
     if (choice == "1") {
-        // Do something...I'm too tired
+        println(unitHeading)
+        val conversion = readln()
+        // used toFloatOrNull and ended up on a wikipedia page about the Billion dollar mistake
+        val finalAnswer = conversion.toFloat() * 3.28084
+        println("$conversion meters -> $finalAnswer feet")
     } else if (choice == "2") {
         // Do something I'm too tired
     } else if (choice == "3") {
